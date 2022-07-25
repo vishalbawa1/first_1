1af6862f9b30f2f32347a1c2e7206c411a452add
vishal <vishald5515@gmail.com>
2022-07-25 16:37:40 -0400

    important

---
+++ day1.py
@@ -0,0 +1,16 @@
+# this is a day one excercise
+print(2+5)
+print(5-4)
+print(5*8)
+print(4/2)
+print(6**2)
+print(6%2)
+
+#checking Data type
+print(type(10))
+print(type(5.11))
+print(type(1+3j))
+print(type('vishal'))
+print(type([1, 2, 3, 4, 5, 6]))
+print(type({'name':'vishal'}))
+print(type({9.8, 6, 5, 8}))
